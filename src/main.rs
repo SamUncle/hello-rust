a9f2b422da87c3461c20b1b44abdb37c7e5592a3
zsm <507772308@qq.com>
2023-11-13 15:44:35 +0800

    秘密数字打印

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-
+    // println!("生成的秘密数字是：{}",secret_number);
     loop {
         println!("Please input your guess.");
 
